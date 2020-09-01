609a25f5da40ad5789ff15947a55bcfc7828563a
Kevin Garcia <kevinalexandergarcia1999@gmail.com>
2020-08-31 23:36:59 -0600

    Corrección rama develop

--- src/app/cargamasiva/cargamasiva.page.spec.ts
+++ src/app/cargamasiva/cargamasiva.page.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { CargamasivaPage } from './cargamasiva.page';
+//import { CargamasivaPage } from './cargamasiva.page';
 
 /*describe('CargamasivaPage', () => {
   let component: CargamasivaPage;
