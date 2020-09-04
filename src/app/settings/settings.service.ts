9b792963f4dc2445023ed64d88521c0e19163f71
Bruno <bcoronado.morales@gmail.com>
2020-09-03 20:02:09 -0600

    Conexión a la base de datos

---
+++ src/app/settings/settings.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Settings } from './settings.interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  private Collection: AngularFirestoreCollection<Settings>;
+  private settings: Observable<Settings[]>;
+
+  constructor(db: AngularFirestore) { 
+    if(db){
+      this.Collection = db.collection<Settings>('usuarios');
+      this.cursos = this.Collection.snapshotChanges().pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return {id, ...data};
+          });
+        })
+      );
+    }
+  }
+}
