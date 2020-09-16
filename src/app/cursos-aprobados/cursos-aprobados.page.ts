1562b90328af153dea60a90c6f8f4c3f4dd735e3
Alan <alanmorataya19@gmail.com>
2020-09-16 16:15:06 -0600

    Se creo una lista dinamica para mostrar los cursos aprobados

--- src/app/cursos-aprobados/cursos-aprobados.page.ts
+++ src/app/cursos-aprobados/cursos-aprobados.page.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos-aprobados.page.scss'],
 })
 export class CursosAprobadosPage implements OnInit {
+  lista:Array<any>=[
+{
+codigo:"017",
+nombre:"Social Humanistica 1"
 
+
+}
+,{
+  codigo:"101",
+  nombre:"Matematica basica 1"
+  
+  
+  }
+  ,{
+    codigo:"069",
+    nombre:"Tecnica Complementaria 1"
+    
+    
+    }
+
+  ]
   constructor() { }
 
   ngOnInit() {
