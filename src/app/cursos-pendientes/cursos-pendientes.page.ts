beed078e73aabd4aa3b4547bab6ec2d08ee1a6f1
Kevin Garcia <kevinalexandergarcia1999@gmail.com>
2020-09-16 23:55:23 -0600

    Método de comparación de cursos terminado

--- src/app/cursos-pendientes/cursos-pendientes.page.ts
+++ src/app/cursos-pendientes/cursos-pendientes.page.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {CursosPendientesService} from './service/cursos-pendientes.service'
+import {Curso} from '../cargamasiva/curso.interface'
+import {CursosAprobados} from '../carga-cursos-aprobados/services/cursos-aprobados.interface'
+
+
 @Component({
   selector: 'app-cursos-pendientes',
   templateUrl: './cursos-pendientes.page.html',
   styleUrls: ['./cursos-pendientes.page.scss'],
 })
 export class CursosPendientesPage implements OnInit {
-
+  aprobados:CursosAprobados;
+  arreglo:[]
   constructor(private servicio:CursosPendientesService) { }
 
   ngOnInit() {
@@ -17,7 +22,32 @@ export class CursosPendientesPage implements OnInit {
   verificarCursos(){
     this.servicio.obtenerCursos().subscribe(res1=>{
       this.servicio.obtenerCursosAprobados().subscribe(res2=>{
-        this.servicio.comparacionCursos(res1,res2)
+
+        let array1=[];
+        let array2=[];
+        let array3=[];
+        
+        for(let i=0;i<res1.length;i++){
+          array1.push(res1[i].payload.doc.data())
+        }
+        
+        for(let i=0;i<res2.length;i++){
+          array2.push(res2[i].payload.doc.data())
+        }
+        
+
+
+
+        for(let i=0;i<array2.length;i++){
+          //if(array2[i].carnetEstudiante==this.carnet){
+            for(let j=0;j<array2[0].cursosAprobados.length;j++){
+              array3.push(array2[0].cursosAprobados[j])
+            }
+          //}
+        }
+
+        let tmp=this.servicio.comparacionCursos(array1,array3)
+        console.log(tmp)
       })
     })
   }
--- src/app/cursos-pendientes/service/cursos-pendientes.service.ts
+++ src/app/cursos-pendientes/service/cursos-pendientes.service.ts
@@ -20,8 +20,33 @@ export class CursosPendientesService {
     return this.firestore.collection("cursos").snapshotChanges();
   }
 
-  comparacionCursos(arreglo1:any[],arreglo2:any[]){
+  comparacionCursos(arreglo1:any[],arreglo2:any[]):any[]{
+    //Arreglo1 = arreglo cursos de sistemas
+    //Arreglo2= arreglo de cursos aprobados
+    let booleano=0;
+    let retorno=[];
     
+    for(let i=0;i<arreglo1.length;i++){
+      
+      for(let j=0;j<arreglo2.length;j++){
+
+        if(arreglo1[i].codigo==arreglo2[j]){
+          booleano=1;
+          break;
+        }    
+
+      }
+
+      if(booleano==0){
+        retorno.push(arreglo1[i]);
+      }else{
+        booleano=0;
+      }
+
+    }
+
+
+    return retorno;
   }
 
   verificarCodigoCurso (codigo: any):boolean {
