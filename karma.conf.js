437bcad363b68f7ff372fb4669476893fc6456d6
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-10-22 13:01:52 -0600

    Prueba de CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-/*
+//const puppeteer = require('puppeteer');
+//process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -40,47 +40,3 @@ module.exports = function (config) {
   });
 };
 
-*/
-
-
-module.exports = function(config) {
-  config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma")
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "../coverage/jenkins-test-app"),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ["progress", "kjhtml"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["Chrome", "ChromeHeadless"],
-    singleRun: false,
-    restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: "Chrome",
-        flags: [
-          "--headless",
-          "--disable-gpu",
-          "--no-sandbox",
-          "--remote-debugging-port=9222"
-        ],
-        
-      },
-    }
-  });
-};
