eae97b00ec9c7ca55b0a1c56d353177afe5b7ac6
Alan <alanmorataya19@gmail.com>
2020-10-26 19:49:07 -0600

    Se añadio campo de correo del usuario logueado al arreglo de horario

--- src/app/crear-horario/crear-horario.page.ts
+++ src/app/crear-horario/crear-horario.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {CrearHorarioService} from './service/crear-horario.service'
-import {CursoHorario, dia} from './service/horario'
-import {Horario} from './service/horario'
-import {CursosAprobados} from '../carga-cursos-aprobados/services/cursos-aprobados.interface'
-import {Curso} from '../cargamasiva/curso.interface'
-import { ToastController,ModalController } from '@ionic/angular';
+import { CrearHorarioService } from './service/crear-horario.service'
+import { CursoHorario, dia } from './service/horario'
+import { Horario } from './service/horario'
+import { CursosAprobados } from '../carga-cursos-aprobados/services/cursos-aprobados.interface'
+import { Curso } from '../cargamasiva/curso.interface'
+import { ToastController, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { User } from '../shared/user.interface';
@@ -17,49 +17,49 @@ import { forkJoin, from, VirtualTimeScheduler } from 'rxjs';
   styleUrls: ['./crear-horario.page.scss'],
 })
 export class CrearHorarioPage implements OnInit {
-  user: User= {uid:'', email:'', displayName:''};
-  aprobados:CursosAprobados;
-  arregloCursos:Curso[]=[];
-  arregloCursos2:Curso[]=[];
+  user: User = { uid: '', email: '', displayName: '' };
+  aprobados: CursosAprobados;
+  arregloCursos: Curso[] = [];
+  arregloCursos2: Curso[] = [];
 
-  cursos:any[]=[];
+  cursos: any[] = [];
 
 
-  diaCurso:dia={
-    horaInicio:"",
-    horaFinal:"",
-    dia:""
+  diaCurso: dia = {
+    horaInicio: "",
+    horaFinal: "",
+    dia: ""
   }
 
-  cursoHorario:CursoHorario={
-    codigoCurso:"",
-    nombreCurso:"",
-    dias:[]
+  cursoHorario: CursoHorario = {
+    codigoCurso: "",
+    nombreCurso: "",
+    dias: []
   }
 
-  arregloCursosHorario:CursoHorario[]=[];
+  arregloCursosHorario: CursoHorario[] = [];
 
 
 
-  cursosPosibles:Curso[]=[]
+  cursosPosibles: Curso[] = []
 
-  curso:Curso={
-    codigo:"",
-    nombre:"",
-    cursospre:[],
-    creditos:"",
-    creditospre:""
+  curso: Curso = {
+    codigo: "",
+    nombre: "",
+    cursospre: [],
+    creditos: "",
+    creditospre: ""
   };
 
-  constructor(private userService: UserService,private servicio:CrearHorarioService,public toastController: ToastController,   public modalCtrl : ModalController,  private router: Router,private storage: Storage) { }
+  constructor(private userService: UserService, private servicio: CrearHorarioService, public toastController: ToastController, public modalCtrl: ModalController, private router: Router, private storage: Storage) { }
 
   ngOnInit() {
     let response = this.userService.getCurrentUser().then(function (firebaseUser) {
       console.log("Encontrado!");
       return firebaseUser;
     });
-    const observable= from(response);
-    observable.subscribe(res => (this.user={uid: res.uid, email: res.email, displayName:res.displayName}));
+    const observable = from(response);
+    observable.subscribe(res => (this.user = { uid: res.uid, email: res.email, displayName: res.displayName }));
     this.comparacionCursosPendientes();
   }
 
@@ -69,56 +69,56 @@ export class CrearHorarioPage implements OnInit {
     let modalSitio = this.modalCtrl.create( 'ModalDetalleSitioPage', sitio );
     modalSitio.present();
  }*/
-  comparacionCursosPendientes(){
+  comparacionCursosPendientes() {
 
 
-    this.servicio.obtenerCursos().subscribe(res1=>{
-      this.servicio.obtenerCursosAprobados().subscribe(res2=>{
+    this.servicio.obtenerCursos().subscribe(res1 => {
+      this.servicio.obtenerCursosAprobados().subscribe(res2 => {
 
-        let array1=[];
-        let array2=[];
-        let array3=[];
-        
-        for(let i=0;i<res1.length;i++){
-          let obj=res1[i].payload.doc.data();
+        let array1 = [];
+        let array2 = [];
+        let array3 = [];
+
+        for (let i = 0; i < res1.length; i++) {
+          let obj = res1[i].payload.doc.data();
           array1.push(obj)
         }
-        
-        
-        for(let i=0;i<res2.length;i++){
-          let obj=res2[i].payload.doc.data();
+
+
+        for (let i = 0; i < res2.length; i++) {
+          let obj = res2[i].payload.doc.data();
           array2.push(obj)
-          
+
 
         }
-        
 
 
 
-        for(let i=0;i<array2.length;i++){
-          if(array2[i].carnetEstudiante==this.user.email){
-            for(let j=0;j<array2[0].cursosAprobados.length;j++){
+
+        for (let i = 0; i < array2.length; i++) {
+          if (array2[i].carnetEstudiante == this.user.email) {
+            for (let j = 0; j < array2[0].cursosAprobados.length; j++) {
               array3.push(array2[0].cursosAprobados[j])
               this.cursos.push(array2[0].cursosAprobados[j]);
             }
           }
         }
 
-        let tmp=this.servicio.comparacionCursos(array1,array3)
-        
-        for(let z=0;z<tmp.length;z++){
-          this.curso={
-            codigo:"",
-            nombre:"",
-            cursospre:[],
-            creditos:"",
-            creditospre:""
+        let tmp = this.servicio.comparacionCursos(array1, array3)
+
+        for (let z = 0; z < tmp.length; z++) {
+          this.curso = {
+            codigo: "",
+            nombre: "",
+            cursospre: [],
+            creditos: "",
+            creditospre: ""
           };
-          this.curso.codigo=tmp[z].codigo;
-          this.curso.nombre=tmp[z].nombre;
-          this.curso.cursospre=tmp[z].cursospre;
-          this.curso.creditos=tmp[z].creditos;
-          this.curso.creditospre=tmp[z].creditospre;
+          this.curso.codigo = tmp[z].codigo;
+          this.curso.nombre = tmp[z].nombre;
+          this.curso.cursospre = tmp[z].cursospre;
+          this.curso.creditos = tmp[z].creditos;
+          this.curso.creditospre = tmp[z].creditospre;
 
 
           this.arregloCursos.push(this.curso);
@@ -132,16 +132,16 @@ export class CrearHorarioPage implements OnInit {
 
   }
 
-  cursosDesbloqueados(){
+  cursosDesbloqueados() {
     //this.cursos = Cursos aprobados
     //this.arregloCursos = Cursos pendientes
- 
 
-    for(let i=0;i<this.arregloCursos.length;i++){
-      let obj=this.arregloCursos[i].cursospre;
-      
 
-      if(this.cursosPreAprobados(obj)){
+    for (let i = 0; i < this.arregloCursos.length; i++) {
+      let obj = this.arregloCursos[i].cursospre;
+
+
+      if (this.cursosPreAprobados(obj)) {
 
         this.arregloCursos2.push(this.arregloCursos[i]);
       }
@@ -150,88 +150,88 @@ export class CrearHorarioPage implements OnInit {
   }
 
 
-  cursosPreAprobados(arreglo:any []):boolean{
+  cursosPreAprobados(arreglo: any[]): boolean {
 
 
-    if(arreglo.length==0)return true;
+    if (arreglo.length == 0) return true;
 
-    let boolean=false;
-    for(let i=0;i<arreglo.length;i++){
+    let boolean = false;
+    for (let i = 0; i < arreglo.length; i++) {
 
-      for(let j=0;j<this.cursos.length;j++){
-        
-       
-        
+      for (let j = 0; j < this.cursos.length; j++) {
 
-        if(String(arreglo[i])==String(this.cursos[j])){
-          boolean=true;
+
+
+
+        if (String(arreglo[i]) == String(this.cursos[j])) {
+          boolean = true;
           break;
         }
       }
 
-      if(boolean==false){
+      if (boolean == false) {
         return false;
-      }else{
-        boolean=false;
+      } else {
+        boolean = false;
       }
 
     }
-    
+
     return true;
   }
 
-  agregarCurso(codigo,nombre,horaInicio,horaFinal,dias){
-    
-   
+  agregarCurso(codigo, nombre, horaInicio, horaFinal, dias) {
+
+
   }
 
-  async crearHorario(){
+  async crearHorario() {
 
 
 
 
-    let horario:Horario={
-      correoEstudiante:"1",
-    cursos:[]
+    let horario: Horario = {
+      correoEstudiante: this.user.email,
+      cursos: []
     }
 
-    for(let i=0;i<this.arregloCursos2.length;i++){
-      await this.storage.get('array'+this.arregloCursos2[i].codigo).then((val) => {
-        if(val!=null && val!= undefined){
+    for (let i = 0; i < this.arregloCursos2.length; i++) {
+      await this.storage.get('array' + this.arregloCursos2[i].codigo).then((val) => {
+        if (val != null && val != undefined) {
           console.log(val)
-          
 
 
-          this.cursoHorario={
-            codigoCurso:"",
-            nombreCurso:"",
-            dias:[]
+
+          this.cursoHorario = {
+            codigoCurso: "",
+            nombreCurso: "",
+            dias: []
           }
-      
-          
 
-          let arraydia=[];
-      
-          for(let j=0;j<val.length;j++){
-            this.diaCurso={
-              horaInicio:"",
-              horaFinal:"",
-              dia:""
+
+
+          let arraydia = [];
+
+          for (let j = 0; j < val.length; j++) {
+            this.diaCurso = {
+              horaInicio: "",
+              horaFinal: "",
+              dia: ""
             }
-            this.diaCurso={
-              horaInicio:val[j].inicio,
-              horaFinal:val[j].fin,
-              dia:val[j].value
+            this.diaCurso = {
+              horaInicio: val[j].inicio,
+              horaFinal: val[j].fin,
+              dia: val[j].value
             }
             arraydia.push(this.diaCurso)
           }
 
-          this.cursoHorario={
-            codigoCurso:this.arregloCursos2[i].codigo,
-            nombreCurso:this.arregloCursos2[i].nombre,
-            dias:arraydia
+          this.cursoHorario = {
+            codigoCurso: this.arregloCursos2[i].codigo,
+            nombreCurso: this.arregloCursos2[i].nombre,
+            dias: arraydia
           }
-      
+
           this.arregloCursosHorario.push(this.cursoHorario);
 
 
@@ -239,32 +239,32 @@ export class CrearHorarioPage implements OnInit {
       });
 
 
-     
+
 
     }
 
     console.log("areglo finaaaaaaaaaaaaaaaaaaaaal")
     console.log(this.arregloCursosHorario)
-    horario.cursos=this.arregloCursosHorario;
-
+    horario.cursos = this.arregloCursosHorario;
+    console.log("SE LE VA ENVIAR CORREO A EL USUARIO" + this.user.email)
     await this.limpiarLocalStorage();
 
     this.servicio.insertar(horario)
 
   }
 
-  async limpiarLocalStorage(){
+  async limpiarLocalStorage() {
 
-    for(let i=0;i<this.arregloCursos2.length;i++){
+    for (let i = 0; i < this.arregloCursos2.length; i++) {
       console.log(this.arregloCursos2[i].codigo)
-        this.storage.set('array'+this.arregloCursos2[i].codigo,null);
+      this.storage.set('array' + this.arregloCursos2[i].codigo, null);
     }
 
   }
 
   agregarCursoPage(id) {
     console.log(id)
-    this.storage.set('id-curso',id);
+    this.storage.set('id-curso', id);
     this.router.navigate(['/crear-horario/add-curso']);
   }
 
